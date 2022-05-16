69edb667f1ec09f6f201ffeffa851ff78cd579f6
Mateus.Cambraia <mateuscambraia.0246@aluno.una.br>
2022-05-16 13:22:25 -0300

    constants

---
+++ ProjetoTest/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoTest
+{
+    public class SupplierQrCodeVariables
+    {
+        public const string CPNJ = "%CPNJ%";
+        public const string CEP = "%CEP%";
+        public const string DATACADASTRO = "%DATACADASTRO%";
+    }
+}
